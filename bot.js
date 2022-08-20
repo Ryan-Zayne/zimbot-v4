2c7cff6778f2e10fd09fb9e9b7e4ffb5c4f9b8dd
Ryan Zayne <93886198+Ryan-Zayne@users.noreply.github.com>
2022-08-20 14:06:20 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,17 +33,17 @@ global.APIKeys = {
 }
 
 //settings
-global.owner = ['27634090203',''] //owner number +27634090203
-global.wame = ['27634090203'] 
-global.pemilik = ['27634090203'] //owner number +27634090203
-global.premium = ['27634090203'] //premium number +27634090203
-global.pengguna = 'Drips' //username
+global.owner = ['2349027092297','2349011894746'] //owner number +27634090203
+global.wame = ['2349027092297','2349011894746'] 
+global.pemilik = ['2349027092297'] //owner number +27634090203
+global.premium = ['2349027092297'] //premium number +27634090203
+global.pengguna = 'Zayne Aguero' //username
 global.footer = 'ZIM BOT INC'
-global.botnma = 'ZIM BOT INC' //bot name
-global.botname = 'ZIM BOT V4'
-global.ownernma = 'Drips' //owner name
-global.packname = 'Drips' //sticker package name
-global.author = 'By Drips Memes' //sticker author name
+global.botnma = 'GENOS'//bot name
+global.botname 'GENOS'
+global.ownernma = 'Zayne Aguero' //owner name
+global.packname = 'Aguero' //sticker package name
+global.author = 'By Zayne Memes' //sticker author name
 global.sessionName = 'session' //session name
 global.prefa = ['#','!','/',''] //prefix
 global.sp = '🔵' //sp
